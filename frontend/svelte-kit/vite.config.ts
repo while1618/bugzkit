17582e9cdd2c2c9ae1b9b50db0bc05a720929f62
Dejan Zdravkovic <dejan.zdravkovic@protonmail.com>
2025-02-28 01:11:41 +0100

    expose env vars in docker compose

--- frontend/svelte-kit/vite.config.ts
+++ frontend/svelte-kit/vite.config.ts
@@ -4,6 +4,10 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   plugins: [paraglide({ project: './project.inlang', outdir: './src/lib/paraglide' }), sveltekit()],
+  server: {
+    port: process.env.PORT ? parseInt(process.env.PORT) : 5173,
+    strictPort: true,
+  },
   test: {
     include: ['src/**/*.{test,spec}.{js,ts}'],
   },
