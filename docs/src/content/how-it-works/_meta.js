4a630fd5bb4a4c68fe09de2bf17687302bc05980
Dejan Zdravkovic <dejan.zdravkovic@protonmail.com>
2026-02-15 18:24:33 +0100

    docs

--- docs/src/content/how-it-works/_meta.js
+++ docs/src/content/how-it-works/_meta.js
@@ -2,7 +2,10 @@ const config = {
   introduction: 'Introduction',
   'project-structure': 'Project Structure',
   auth: 'Auth',
+  'error-handling': 'Error Handling',
+  i18n: 'Internationalization',
   email: 'Email',
+  testing: 'Testing',
 };
 
 export default config;
