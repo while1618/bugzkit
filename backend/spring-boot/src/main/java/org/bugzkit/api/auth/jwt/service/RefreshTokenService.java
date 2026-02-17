29af3ab6e1e5df0f129df34f90c314c24096c511
Dejan Zdravkovic <dejan.zdravkovic@protonmail.com>
2026-02-17 01:54:38 +0100

    remove findByuserIdAndDeviceId

--- backend/spring-boot/src/main/java/org/bugzkit/api/auth/jwt/service/RefreshTokenService.java
+++ backend/spring-boot/src/main/java/org/bugzkit/api/auth/jwt/service/RefreshTokenService.java
@@ -1,6 +1,5 @@
 package org.bugzkit.api.auth.jwt.service;
 
-import java.util.Optional;
 import java.util.Set;
 import org.bugzkit.api.user.payload.dto.RoleDTO;
 
@@ -8,8 +7,6 @@ public interface RefreshTokenService {
   String create(Long userId, Set<RoleDTO> roleDTOs, String deviceId);
 
   void check(String token);
-
-  Optional<String> findByUserIdAndDeviceId(Long userId, String deviceId);
 
   void delete(String token);
 
