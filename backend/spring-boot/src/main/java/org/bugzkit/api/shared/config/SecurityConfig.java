d4cedc6ef4b70b664b9833141aa128abe2595bd2
Dejan Zdravkovic <dejan.zdravkovic@protonmail.com>
2026-02-24 10:31:41 +0100

    BCryptPasswordEncoder strength

--- backend/spring-boot/src/main/java/org/bugzkit/api/shared/config/SecurityConfig.java
+++ backend/spring-boot/src/main/java/org/bugzkit/api/shared/config/SecurityConfig.java
@@ -65,7 +65,7 @@ public class SecurityConfig {
 
   @Bean
   public PasswordEncoder bCryptPasswordEncoder() {
-    return new BCryptPasswordEncoder();
+    return new BCryptPasswordEncoder(12);
   }
 
   @Bean
