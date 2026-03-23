ba6e6d92ebed9b30a381e24151f5e13d032314ab
Dejan Zdravkovic <dejan.zdravkovic@protonmail.com>
2026-03-23 11:01:05 +0100

    remove profile annotation from RedisConfig

--- backend/spring-boot/src/main/java/org/bugzkit/api/shared/config/RedisConfig.java
+++ backend/spring-boot/src/main/java/org/bugzkit/api/shared/config/RedisConfig.java
@@ -6,7 +6,6 @@ import java.time.Duration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisPassword;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
